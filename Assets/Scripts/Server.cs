5d301bcb88255c556b3e5d99d1f8c6e8fcd2c222
Christian Sejer <christian_sejer@hotmail.com>
2020-10-28 16:59:55 +0100

    Various fixes. Server side collisions

--- Assets/Scripts/Server.cs
+++ Assets/Scripts/Server.cs
@@ -123,4 +123,10 @@ public class Server
 
         Debug.Log("Initialized packets");
     }
+
+    public static void Stop()
+    {
+        tcpListener.Stop();
+        udpListener.Close();
+    }
 }
